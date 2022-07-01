eb10e1bdd7644a50a2cf446c9799542b6d288f1f
Mohammed Taha Alibhai <mot1922092@sicsr.ac.in>
2022-07-01 22:54:27 +0300

    Update size_config.dart
    The init function initializes our screenWidth and screenWeight variables based on the device it is currently running on.
    
    The variables are fetched from the context which is then stored in _mediaQueryData.

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -6,4 +6,11 @@ class SizeConfig {
   static double screenHeight;
   static double  defaultSize;
   static Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
 }
